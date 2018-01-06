08ca0d44357465394e7e94f8102ef9e194fc3305
Rumata <rokachov@gmail.com>
2018-01-06 12:21:23 +0200

    refactor

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,6 +1,4 @@
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 class Triangle {
     private static final int SCALENE = 1;
@@ -14,20 +12,21 @@ class Triangle {
      * 1- if it is a scalene triangle, 2 - for isosceles triangle,
      * 3 - for equilateral triangle, 4 - if it is not a triangle or if was
      * entered negative number or null
+     *
      * @param a, a length of first the triangle side
      * @param b, a length of second the triangle side
      * @param c, a length of second the triangle side
      * @return integer value of triangle type
      */
     int getTriangleType(int a, int b, int c) {
-        int[] array = {a,b,c};
+        int[] array = {a, b, c};
         Arrays.sort(array);
         a = array[0];
         b = array[1];
         c = array[2];
         if (a <= 0) return ERROR;
         if ((a + b) <= c) return ERROR;
-        if (a == b && b==c) return EQUILATERAL;
+        if (a == b && b == c) return EQUILATERAL;
         else {
             if (a == b || b == c) return ISOSCELES;
             else return SCALENE;
