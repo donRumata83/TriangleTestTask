0a7514c653eb2d1584285fa7bb7f2ce406eb62d7
Rumata <rokachov@gmail.com>
2018-01-06 11:33:01 +0200

    Tests

---
+++ src/main/java/Triangle.java
@@ -0,0 +1,6 @@
+public class Triangle {
+
+    public int getTriangleType(int a, int b, int c) {
+        return 0;
+    }
+}
---
+++ src/test/java/TriangleFunctionTests.java
@@ -0,0 +1,69 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class TriangleFunctionTests {
+    private static final int SCALENE = 1;
+    private static final int ISOSCELES = 2;
+    private static final int EQUILATERAL = 3;
+    private static final int ERROR = 4;
+    public Triangle triangle;
+
+    @Before
+    public void setTriangle(){
+        triangle = new Triangle();
+    }
+    @Test
+    public void equilateralTriangleTest() {
+        int triangleType = triangle.getTriangleType(2, 2, 2);
+
+        assertThat(triangleType, is(equalTo(EQUILATERAL)));
+    }
+
+
+    @Test
+    public void isoscelesTriangleTest() {
+        int triangleType = triangle.getTriangleType(5,2,2);
+
+        assertThat(triangleType, is(equalTo(ISOSCELES)));
+    }
+
+    @Test
+    public void scaleneTriangleTest(){
+        int triangleType = triangle.getTriangleType(2,3,4);
+
+        assertThat(triangleType, is(equalTo(SCALENE)));
+    }
+
+    @Test
+    public void nullEnterTest(){
+        int triangleType = triangle.getTriangleType(1,2,0);
+
+        assertThat(triangleType, is(equalTo(ERROR)));
+    }
+
+    @Test
+    public void negativeNumberEnterTest() {
+        int triangleType = triangle.getTriangleType(-1, -1, 2);
+
+        assertThat(triangleType, is(equalTo(ERROR)));
+    }
+
+    @Test
+    public void notTriangleEnterTest(){
+        int triangleType = triangle.getTriangleType(1,2,5);
+
+        assertThat(triangleType, is(equalTo(ERROR)));
+    }
+
+    @Test
+    public void testWhenLineLehgthEnter(){
+        int triangleType = triangle.getTriangleType(3, 2, 1);
+
+        assertThat(triangleType, is(equalTo(ERROR)));
+    }
+
+}
